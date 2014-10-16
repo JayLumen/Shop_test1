7632882e4b5710578d7e81b5482c48505a0d0cc1
JayLumen <jaylumen2@gmail.com>
2014-10-16 20:30:49 +0300

    Added entities.

---
+++ src/main/java/ua/shop/entities/Good.java
@@ -0,0 +1,23 @@
+package ua.shop.entities;
+
+
+public class Good {
+    private String name;
+    private int prize;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public void setPrize(int prize) {
+        this.prize = prize;
+    }
+}
--- src/main/java/ua/shop/entities/Member.java
+++
@@ -1,7 +0,0 @@
-package ua.shop.entities;
-
-/**
- * Created by Jay on 14.10.2014.
- */
-public class Member {
-}

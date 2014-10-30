f51be052a43e225dde7b7198f7c99f7ed9fbed3c
JayLumen <jaylumen2@gmail.com>
2014-10-30 20:37:11 +0200

    Added entities in one mapping, view on one page.

--- src/main/java/ua/shop/controllers/SimpleController.java
+++ src/main/java/ua/shop/controllers/SimpleController.java
@@ -4,9 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import ua.shop.entities.Good;
+import ua.shop.entities.RealBrand;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class SimpleController {
@@ -17,7 +20,15 @@ public class SimpleController {
         Good good2 = new Good("TV", 1800);
         goods.add(good1);
         goods.add(good2);
-        return new ModelAndView("goods", "list_of_goods", goods);
+        List<RealBrand> brands = new ArrayList<RealBrand>();
+        RealBrand brand1 = new RealBrand("Samsung", 1000, 2);
+        RealBrand brand2 = new RealBrand("LG", 1050, 5);
+        brands.add(brand1);
+        brands.add(brand2);
+        Map<String, List<? extends Object>> models = new HashMap<String, List<? extends Object>>();
+        models.put("list_of_goods", goods);
+        models.put("list_of_brand", brands);
+      return new ModelAndView("goods", models);
     }
 
     @RequestMapping("/goods2.do")
@@ -29,4 +40,6 @@ public class SimpleController {
         goods.add(good2);
         return new ModelAndView("goods", "list_of_goods", goods);
     }
+
+
 }
--- src/main/webapp/WEB-INF/views/goods.jsp
+++ src/main/webapp/WEB-INF/views/goods.jsp
@@ -2,20 +2,26 @@
 <%@ taglib uri="http://java.sun.com/jstl/core_rt" prefix="c" %>
 <html>
 <head>
-    List of goods <br/>
+    <h4>
+        <center>List of goods</center>
+    </h4>
+    <br/>
     <c:forEach items="${list_of_goods}" var="good">
         name: [<c:out value="${good.name}"/>]
         <br/>
         prize: [<c:out value="${good.prize}"/>]
         <br/>
     </c:forEach>
-    List of brand <br/>
+    <h4>
+        <center>List of brand</center>
+    </h4>
+    <br/>
     <c:forEach items="${list_of_brand}" var="brand">
-        name: [<c:out value="${brand.name}"/>
+        name: [<c:out value="${brand.name}"/>]
         <br/>
-        price: [<c:out value="${brand.price}"/>
+        price: [<c:out value="${brand.price}"/>]
         <br/>
-        quantity: [<c:out value="${brand.quantity}"/>
+        quantity: [<c:out value="${brand.quantity}"/>]
         <br/>
 
     </c:forEach>
